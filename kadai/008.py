3afc938524baead455d887a2405b9319b6b5854c
saepin <npino1222@outlook.co.jp>
2023-06-24 17:29:41 +0900

    kadai_008

---
+++ kadai/008.py
@@ -0,0 +1,11 @@
+for ver in range(1,101):
+   if ver % 3 == 0 and ver % 5 == 0:
+    print("FIZBUZZ")
+   elif ver % 3 ==0:
+       print("Fizz")
+   elif ver % 5 ==0:
+       print("BUZZ")
+   else:
+      print(ver)
+
+   
